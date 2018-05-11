f29a41b6b3dcbb86e09bcf48f8589f5b74b1f5d0
Unknown <darkspark52@gmail.com>
2018-05-10 21:57:41 -0400

    Started working on layout
    Got a few things for the layout going, needs more functionality added and to be better positioned, but this is just to get things in the interface.

---
+++ Assets/Scripts/InfectCounter.cs
@@ -0,0 +1,32 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+using UnityEngine.UI;
+
+public class InfectCounter : MonoBehaviour {
+
+    public Text infectTotal;
+    int infect;
+
+	// Use this for initialization
+	void Start () {
+		
+	}
+	
+	// Update is called once per frame
+	void Update () {
+		
+	}
+
+    public void InfectUp()
+    {
+        infect++;
+        infectTotal.text = infect.ToString();
+    }
+
+    public void InfectDown()
+    {
+        infect--;
+        infectTotal.text = infect.ToString();
+    }
+}
---
+++ Assets/Scripts/LifeCounter.cs
@@ -0,0 +1,31 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+using UnityEngine.UI;
+
+public class LifeCounter : MonoBehaviour {
+
+	public Text lifeText;
+	int life = 20;
+
+	// Use this for initialization
+	void Start () {
+		lifeText.text = life.ToString();
+	}
+	
+	// Update is called once per frame
+	void Update () {
+		
+	}
+
+	public void LifeUp(){
+		life++;
+		lifeText.text = life.ToString();
+	}
+
+	public void LifeDown(){
+		life--;
+		lifeText.text = life.ToString();
+	}
+
+}
