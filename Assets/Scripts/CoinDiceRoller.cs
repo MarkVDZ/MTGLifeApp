61222fbdee47056a0797d153455199b027c305a6
Unknown <darkspark52@gmail.com>
2018-06-01 18:55:02 -0400

    Added some side features
    - Made a simple mana counter
    - Made a coin flipper/multiple dice roller

---
+++ Assets/Scripts/CoinDiceRoller.cs
@@ -0,0 +1,69 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+using UnityEngine.UI;
+
+public class CoinDiceRoller : MonoBehaviour {
+
+    public Text rollResultText;
+    public int result;
+
+	// Use this for initialization
+	void Start () {
+		
+	}
+	
+	// Update is called once per frame
+	void Update () {
+		
+	}
+
+    public void Roll(string type)
+    {
+        switch (type)
+        {
+            case "coin":
+                result = Random.Range(0, 2);
+                if (result == 0) {
+
+                    rollResultText.text = "heads"; 
+                }
+                else
+                {
+                    rollResultText.text = "tails";
+                }  
+                break;
+            case "3":
+                result = Random.Range(1, 4);
+                rollResultText.text = result.ToString();
+                break;
+            case "6":
+                result = Random.Range(1, 7);
+                rollResultText.text = result.ToString();
+                break;
+            case "8":
+                result = Random.Range(1, 9);
+                rollResultText.text = result.ToString();
+                break;
+            case "10":
+                result = Random.Range(1, 11);
+                rollResultText.text = result.ToString();
+                break;
+            case "12":
+                result = Random.Range(1, 13);
+                rollResultText.text = result.ToString();
+                break;
+            case "20":
+                result = Random.Range(1, 21);
+                rollResultText.text = result.ToString();
+                break;
+            case "100":
+                result = Random.Range(1, 101);
+                rollResultText.text = result.ToString();
+                break;
+        }
+        //result = Random.Range(1, 6);
+        //rollResultText.text = result.ToString();
+    }
+
+}
---
+++ Assets/Scripts/ManaCounterController.cs
@@ -0,0 +1,102 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+using UnityEngine.UI;
+
+public class ManaCounterController : MonoBehaviour {
+
+    public Text whiteManaText;
+    public Text blueManaText;
+    public Text redManaText;
+    public Text greenManaText;
+    public Text blackManaText;
+    public Text colorlessManaText;
+    public Text totalManaText;
+
+    int totalWhiteMana;
+    int totalBlueMana;
+    int totalRedMana;
+    int totalGreenMana;
+    int totalBlackMana;
+    int totalColorlessMana;
+    int totalMana;
+
+	// Use this for initialization
+	void Start () {
+		
+	}
+	
+
+    private void UpdateTotal()
+    {
+        totalMana = totalBlackMana + totalBlueMana + totalColorlessMana + totalGreenMana + totalRedMana + totalWhiteMana;
+        totalManaText.text = totalMana.ToString();
+    }
+
+    public void IncreaseMana(string color)
+    {
+        switch (color)
+        {
+            case "white":
+                totalWhiteMana++;
+                whiteManaText.text = totalWhiteMana.ToString();
+                break;
+            case "blue":
+                totalBlueMana++;
+                blueManaText.text = totalBlueMana.ToString();
+                break;
+            case "red":
+                totalRedMana++;
+                redManaText.text = totalRedMana.ToString();
+                break;
+            case "green":
+                totalGreenMana++;
+                greenManaText.text = totalGreenMana.ToString();
+                break;
+            case "black":
+                totalBlackMana++;
+                blackManaText.text = totalBlackMana.ToString();
+                break;
+            case "colorless":
+                totalColorlessMana++;
+                colorlessManaText.text = totalColorlessMana.ToString();
+                break;
+        }
+
+        UpdateTotal();
+    }
+
+    public void DecreaseMana(string color)
+    {
+        switch (color)
+        {
+            case "white":
+                totalWhiteMana--;
+                whiteManaText.text = totalWhiteMana.ToString();
+                break;
+            case "blue":
+                totalBlueMana--;
+                blueManaText.text = totalBlueMana.ToString();
+                break;
+            case "red":
+                totalRedMana--;
+                redManaText.text = totalRedMana.ToString();
+                break;
+            case "green":
+                totalGreenMana--;
+                greenManaText.text = totalGreenMana.ToString();
+                break;
+            case "black":
+                totalBlackMana--;
+                blackManaText.text = totalBlackMana.ToString();
+                break;
+            case "colorless":
+                totalColorlessMana--;
+                colorlessManaText.text = totalColorlessMana.ToString();
+                break;
+        }
+
+        UpdateTotal();
+    }
+
+}
